69ce794774fbc3732fe9deb9781740d18684e62a
Jin Qian <qianj989@newschool.edu>
2016-12-11 18:09:29 +0000

    laozijueduibugaile

---
+++ catalog1/js/main.js
@@ -0,0 +1,3 @@
+$('#button').click(function() {
+    $('#button_feekback').html('button has been clicked.');
+})
---
+++ test/js/main.js
@@ -0,0 +1,9 @@
+$(document) .ready(function(){
+    $('.card1').mouseover(function(){
+        $('.box').show(2000);
+    })
+    
+$('.card1').mouseout(function(){
+    $('.box').hide(2000);
+})
+})
